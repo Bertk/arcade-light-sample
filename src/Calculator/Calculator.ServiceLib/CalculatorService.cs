c0fa64b34424871ac1251d517e60b4a3f91968bd
Bert <bert.k@hotmail.com>
2023-07-08 10:42:52 +0200

    disable S1244

--- src/Calculator/Calculator.ServiceLib/CalculatorService.cs
+++ src/Calculator/Calculator.ServiceLib/CalculatorService.cs
@@ -1,6 +1,6 @@
-﻿using System.Threading.Tasks;
-using Grpc.Core;
+using System.Threading.Tasks;
 using Calculator.SharedLib.Generated;
+using Grpc.Core;
 using static Calculator.SharedLib.Generated.CalculatorService;
 
 namespace Calculator.ServiceLib
@@ -18,10 +18,12 @@ namespace Calculator.ServiceLib
 
         public override Task<CalculatorReply> Subtract(CalculatorRequest request, ServerCallContext context)
         {
+#pragma warning disable S1244 // Floating point numbers should not be tested for equality
             if (request.N2 == 0)
             {
                 throw new RpcException(new Status(StatusCode.InvalidArgument, "Division by zero."));
             }
+#pragma warning restore S1244 // Floating point numbers should not be tested for equality
 
             return Task.FromResult(new CalculatorReply { Result = request.N1 - request.N2 });
         }
